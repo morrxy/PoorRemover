e2060003a3cafbd9fff5cb443be336a734fc0ee8
morrxy <morrxy@gmail.com>
2019-11-11 16:11:56 +0800

    first commit

---
+++ PoorRemover.lua
@@ -0,0 +1,17 @@
+SLASH_REMOVEPOOR1, SLASH_REMOVEPOOR2 = '/removepoor', '/rmpr'
+
+function SlashCmdList.REMOVEPOOR(msg, editBox)
+    for i = 0, 4 do
+        for j = 1, GetContainerNumSlots(i) do
+            local k = GetContainerItemLink(i, j)
+            if k then
+                local _, _, r = GetItemInfo(k)
+                if r == 0 then
+                    PickupContainerItem(i, j)
+                    DeleteCursorItem()
+                    print(k .. "was removed")
+                end
+            end
+        end
+    end
+end
